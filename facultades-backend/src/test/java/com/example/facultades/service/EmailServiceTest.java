a43fae8f97f6466f55e99990ed9060d2403565b3
Your Name <you@example.com>
2025-12-23 13:21:41 -0300

    docker-compose

---
+++ facultades-backend/src/test/java/com/example/facultades/service/EmailServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.facultades.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTest {
+}
